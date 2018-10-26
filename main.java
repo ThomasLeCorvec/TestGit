7d758738756f2f8c3a6632103c0df6f5742a9c73
ThomasLeCorvec <38437176+ThomasLeCorvec@users.noreply.github.com>
2018-10-26 14:40:36 +0200

    Ajout de la fonction sayHello() depuis GitHub

--- main.java
+++ main.java
@@ -10,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    public String sayHello(){
+        System.out.println("Hello World");
+    }
 }
